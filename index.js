4fd9b6781464d6dce3a96d22312896b6f12653ed
pat <pat@getfoodgenius.com>
2020-08-17 12:12:32 -0500

    adding api

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-// index.js
 const Mustache = require('mustache');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
-/**
- * DATA is the object that contains all
- * the data to be provided to Mustache
- * Notice the "name" and "date" property.
- */
-let DATA = {
-  name: 'Pat',
-  date: new Date().toLocaleDateString('en-US', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-  }),
-  today_wave: '2-4',
-  today_wind: '5-8',
-  tomorrow_wind: '1-2',
-  tomorrow_wave: '5-10',
-};
-/**
- * A - We open 'main.mustache'
- * B - We ask Mustache to render our file with the data
- * C - We create a README.md file with the generated output
- */
-function generateReadMe() {
+const fetch = require('node-fetch');
+const forecastURL = 'https://api.weather.gov/gridpoints/LOT/75,72/forecast';
+const windwaveURL = 'https://api.weather.gov/gridpoints/LOT/75,72';
+
+function generateReadMe(INPUT_DATA) {
   fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
+    const output = Mustache.render(data.toString(), INPUT_DATA);
     fs.writeFileSync('README.md', output);
   });
 }
-generateReadMe();
+
+fetch(forecastURL, { method: 'Get' })
+  .then((res) => res.json())
+  .then((json) => {
+    // do something with JSON
+    const forecasts = json['properties']['periods'];
+    fetch(windwaveURL, { method: 'Get' })
+      .then((res) => res.json())
+      .then((json) => {
+        // do something with JSON
+        const wavewind = json['properties']['periods'];
+        let DATA = {
+          name: 'Pat',
+          date: new Date().toLocaleDateString('en-US', {
+            weekday: 'long',
+            month: 'long',
+            day: 'numeric',
+          }),
+          forecasts: forecasts,
+          today_wave: '2-4',
+          today_wind: '5-8',
+          tomorrow_wind: '1-555',
+          tomorrow_wave: '5-10',
+        };
+        generateReadMe(DATA);
+      });
+  });
